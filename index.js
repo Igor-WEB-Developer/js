a5161720261ecd57f87c47ef3f6bd960c97fa911
Igor-WEB-Developer <98714337+Igor-WEB-Developer@users.noreply.github.com>
2022-06-05 14:36:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict";
+// console.log(125);
+// for(let i = 0; i<10; i++)
+// {
+//     console.log(i);
+// }
+// let a = 45;
+
+// let b = 33, c = 66;
+// // alert(Date()); 
+
+
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = `aaa ${str1} bbb ${str2} ccc`;
+
+// let str = `a
+// b
+// c`; 
+// // alert(str)
+
+// console.log("123");
+// console.log(123);
+// console.log(123.5);
+// console.log('a'*'a');
+
+// let r = 5.5;
+// const PI = 3.14;
+// let l = 2 * PI * r;
+// // alert(l);
+
+// let i = '' + 3 + 1;
+// // alert('12345'[4]);
+
+// let aaa = 0.1 + 0.2;
+// // alert(0.3 - 0.1);
+
+
+// // let name1 = prompt('hiii')
+// // console.log(name1)
+
+// // let num1 = prompt('Введите первое число');
+// // let num2 = prompt('Введите второе число');
+
+// // alert(Number(num1) + Number(num2)); // сложит числа как строки
+
+
+// for(i=1; i<6; i++)
+// {
+//     document.write('<b>' + i + '</b><br>');
+// }
+
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ', num1 + num2);
+
+let mas = ['a', 'b', 'c'];
+// alert(mas[mas.length-1]);
+
+for(let i=0; i<mas.length+1; i++)
+{
+    mas[i] = i+2;
+}
+console.log(mas[0])
